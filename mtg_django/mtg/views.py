40ecf7fe39edc53bb20ee149736cabfc17f7adb9
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-08-31 14:55:30 -0400

    Render cards from MTG SDK

--- mtg_django/mtg/views.py
+++ mtg_django/mtg/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from mtgsdk import Card
+from mtgsdk import Set
+from mtgsdk import Type
+from mtgsdk import Supertype
+from mtgsdk import Subtype
+from mtgsdk import Changelog
 
 # Create your views here.
+def main_page(request):
+    card = Card.find(386616)
+    return render(request, 'mtg/main_page.html', {'card': card})
